a4123ddca180c6ff2b2f1ec65b0fcea519bf9fe1
Sudesh Kharel <sudeshkharel15@gmail.com>
2024-12-15 22:35:50 +0545

    added checkloginstatusmethod  binding changed to loginbinding

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:my_flutter_app/routes/app_pages.dart';
 import 'package:alice/alice.dart';
+import 'package:my_flutter_app/screen/auth/login/login_binding.dart';
+import 'package:my_flutter_app/screen/auth/login/login_view.dart';
 //import 'package:my_flutter_app/screen/auth/login/login_binding.dart';
 import 'package:my_flutter_app/screen/shop_home/shop_home_binding.dart';
+import 'package:my_flutter_app/screen/shop_home/shop_home_view.dart';
 // import 'package:my_flutter_app/screen/auth/register/register_binding.dart';
 // import 'package:my_flutter_app/screen/auth/register/register_view.dart';
 // import 'package:my_flutter_app/screen/shop_home/shop_home_binding.dart';
 // import 'package:my_flutter_app/screen/shop_home/shop_home_view.dart';
 import 'package:my_flutter_app/screen/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   await GetStorage.init();
@@ -29,6 +33,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool isLoggedIn = false; // Initially set to false
   final storage = GetStorage();
 
   @override
@@ -39,6 +44,23 @@ class _MyAppState extends State<MyApp> {
       maxCallsCount: 1000,
     );
     super.initState();
+    checkLoginStatus(); // Check login state on app start
+  }
+
+  Future<void> checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isLoggedIn = prefs.getBool('isLoggedIn') ?? false; // Load login state
+    });
+
+    // Navigate based on login status
+    if (isLoggedIn) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => ShopHomePage()));
+    } else {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    }
   }
 
   // This widget is the root of your application.
@@ -58,8 +80,8 @@ class _MyAppState extends State<MyApp> {
               : SplashScreen(),
       initialBinding:
           (storage.read("is_login") != null && storage.read("is_login") == true)
-              ? ShopHomeBinding()
-              : ShopHomeBinding(),
+              ? LoginBinding()
+              : LoginBinding(),
       getPages: AppPages.pages,
     );
   }
