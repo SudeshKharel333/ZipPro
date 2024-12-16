f33d08118332f4ed42d6a8cbd9375f3465063de8
Sudesh Kharel <sudeshkharel15@gmail.com>
2024-12-16 12:30:32 +0545

    deleted files

--- lib/screen/home/SearchScreen/SearchBinding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'SearchLogic.dart';
-
-class Searchbinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => SearchLogic());
-  }
-}
