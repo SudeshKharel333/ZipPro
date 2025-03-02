033d66aab43567509aa3d26fc11aaac116866571
Sudesh Kharel <sudeshkharel15@gmail.com>
2025-03-02 09:14:04 +0545

    deleted unnecessary files

--- lib/screen/cart/cart_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:my_flutter_app/screen/cart/cart_logic.dart';
-
-class CartBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => CartLogic());
-  }
-}
